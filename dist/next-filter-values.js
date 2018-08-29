5bdad239d8271b3a3d87ba6339a951ed65ae4c05
afeiship <1290657123@qq.com>
2018-08-29 18:22:37 +0800

    issue: update key

--- dist/next-filter-values.js
+++ dist/next-filter-values.js
@@ -2,10 +2,11 @@
 
   var global = global || this || window || Function('return this')();
   var nx = global.nx || require('next-js-core2');
+  var DEFAULT_KEY = 'id';
 
   nx.filterValues = function (inArray, inValues, inKey) {
     var result = [];
-    var key = inKey || 'id';
+    var key = inKey || DEFAULT_KEY;
     inValues.forEach(function (id) {
       inArray.forEach(function (item) {
         if (id === item[key]) {
